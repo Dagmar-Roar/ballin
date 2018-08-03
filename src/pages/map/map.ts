693ccd922d081a7b4489122560d3cef8014e9ad8
Dagmar-Roar <iamerwanaubert@gmail.com>
2018-08-03 07:56:45 +0200

    google i ok

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { markParentViewsForCheckProjectedViews } from '@angular/core/src/view/util';
+import { Geolocation } from '@ionic-native/geolocation'
 declare var google;
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
@@ -8,20 +11,47 @@ declare var google;
 export class MapComponent {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController,
+              public geolocation: Geolocation) {}
 
   ionViewDidLoad() {
     this.loadMap();
   }
 
   loadMap() {
-    let latLng = new google.maps.LatLng(48.853898, 2.360173);
-    let mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.geolocation.getCurrentPosition().then( (position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
+  addMarker() {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>INFO</h4>";
+
+    console.log(marker);
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker,content) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker,'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 }
