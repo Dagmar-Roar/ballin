9d2211216db37219d069ca57b6116d63eea86798
Dagmar-Roar <37444883+Dagmar-Roar@users.noreply.github.com>
2018-08-09 18:48:45 +0200

    Update config.ts

--- src/fireConfig/config.ts
+++ src/fireConfig/config.ts
@@ -1,11 +1,6 @@
 
 export const firebaseConfig = {
 	fire: {
-        apiKey: "AIzaSyBdqtambYlXQVy35aOMOZD64Aqd_e3vaXA",
-        authDomain: "ballin-203112.firebaseapp.com",
-        databaseURL: "https://ballin-203112.firebaseio.com",
-        projectId: "ballin-203112",
-        storageBucket: "ballin-203112.appspot.com",
-        messagingSenderId: "934593256703"
+    //your conf
 	}
 };
